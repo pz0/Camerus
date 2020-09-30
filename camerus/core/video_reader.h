e6393fad6e25b7c86506d415774c0e345461cca3
pz0 <pz0@github.com>
2020-09-30 11:45:40 +0200

    video_reader class improved

--- camerus/core/video_reader.h
+++ camerus/core/video_reader.h
@@ -1,14 +1,52 @@
-#include <opencv2/imgproc.hpp>
+#include <thread>
+#include <iostream>
+#include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
+#include "logger.h"
 
 
 namespace camerus::core
 {
-    class VideoReader {
+    class VideoReader 
+    {
         public:
-            VideoReader(int index) : cap(index) {}
-            VideoReader(char *source) : cap(source) {}
+            VideoReader(int index)
+            {
+                finish_thread = false;
+                cap = cv::VideoCapture(index);
+                thread = std::thread(thread_task, this);
+            }
+            
+            VideoReader(char *source)
+            {
+                finish_thread = false;
+                cap = cv::VideoCapture(source);
+                thread = std::thread(thread_task, this);
+            }
+
+            ~VideoReader()
+            {
+                finish_thread = true;
+                thread.join();
+            }
+            
+            void thread_task_body()
+            {
+                while(!finish_thread)
+                {
+                    cap.read(mat);
+                    camerus::core::logger::print_log("DEBUG", "New Frame");
+                }
+            }
         private:
+            static void thread_task(VideoReader *ptr)
+            {
+                ptr->thread_task_body();
+            }
+
             cv::VideoCapture cap;
+            cv::Mat mat;
+            bool finish_thread;
+            std::thread thread;
     };
 }

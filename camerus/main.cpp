4ce9e5e204e22528f8a30026d775200fbed0c2d9
pz0 <pz0@github.com>
2020-09-30 11:46:04 +0200

    cosmetic changes

--- camerus/main.cpp
+++ camerus/main.cpp
@@ -1,10 +1,15 @@
-#include <iostream>
+#include <unistd.h>
 #include "core/video_reader.h"
 
 
 int main(int argc, char *argv[])
 {
     camerus::core::VideoReader reader = camerus::core::VideoReader("test");
+
+    while(1)
+    {
+        usleep(1000);
+    }
 
     return 0;
 }

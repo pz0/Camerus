d4ef656e883f70e5e31309a2479a50851c0d2945
pz0 <pz0@github.com>
2020-10-02 11:41:12 +0200

    processing chain changed

--- camerus/main.cpp
+++ camerus/main.cpp
@@ -2,9 +2,11 @@
 #include <unistd.h>
 #include "core/video_reader.h"
 #include "core/processing/filters.h"
+#include "core/processing/edge_detection.h"
 #include "core/processing/background_extractor_mog2.h"
 #include "core/output/video_creator.h"
 #include "core/output/concatenation.h"
+
 
 
 int main(int argc, char *argv[])
@@ -15,19 +17,35 @@ int main(int argc, char *argv[])
     cv::Mat frame = reader.get_current_frame();
     std::cout << frame.cols << "\n";
     camerus::core::output::VideoCreator video1("/tmp/resources/test.avi", frame.cols * 3, frame.rows, 15);
+    camerus::core::output::VideoCreator video2("/tmp/resources/test2.avi", frame.cols * 3, frame.rows, 15);
+    camerus::core::output::VideoCreator video3("/tmp/resources/test3.avi", frame.cols * 3, frame.rows * 2, 15);
 
     cv::Mat stage1, stage2, stage3;
+    cv::Mat stage4, stage5;
 
     while(!frame.empty())
     {
         stage1 = frame;
-        camerus::core::processing::median_blur(stage1, stage2, 9);
-        stage3 = bge.apply(stage2);
+        camerus::core::processing::gaussian_blur(stage1, stage1, 5);
+        stage2 = bge.apply(stage1);
+        camerus::core::processing::median_blur(stage2, stage3, 9);
 
-        camerus::core::output::concatenation::horizontal_concat(stage1, stage2, stage2);
-        camerus::core::output::concatenation::horizontal_concat(stage2, stage3, stage3);
+        // from left: input frame (with gaussian blur) -> background subtraction -> median blur
+        cv::Mat concat_stage1, concat_stage2;
+        camerus::core::output::concatenation::horizontal_concat(stage1, stage2, concat_stage1);
+        camerus::core::output::concatenation::horizontal_concat(concat_stage1, stage3, concat_stage1);
+        video1.write(concat_stage1);
 
-        video1.write(stage3);
+        stage4 = camerus::core::processing::detect_canny_edges(stage3, 100, 100);
+        stage5 = camerus::core::processing::draw_canny_edges_on(stage4, frame, 0.25f);
+
+        camerus::core::output::concatenation::horizontal_concat(stage3, stage4, concat_stage2);
+        camerus::core::output::concatenation::horizontal_concat(concat_stage2, stage5, concat_stage2);
+        video2.write(concat_stage2);
+
+        camerus::core::output::concatenation::vertical_concat(concat_stage1, concat_stage2, concat_stage2);
+        video3.write(concat_stage2);
+
         frame = reader.get_current_frame();
     }
 
